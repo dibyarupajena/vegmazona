1bd087b38d0b252b949b42e7479e55d0a1153510
dibyarupa jena <dibyathecool.jena@gmail.com>
2020-08-27 00:23:37 +0530

    Frontend done

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -2,19 +2,20 @@ export default {
     products: [
     {   
         _id:'1',
-        name: 'Tomato',
-        Category: 'Vegetables and fruits',
-        image: '/images/d1.jpg',
-        price: 60,
+        name: 'Samosa',
+        Category: 'Fast food',
+        image: '/images/d4.jpg',
+        price: 25,
         brand: 'Reliance',
-        rating: 4.5,
-        numReviews: 10
+        rating: 3.0,
+        numReviews: 6
+        
     },
     {
         _id:'2',
         name: 'Potato',
         Category: 'Vegetables and fruits',
-        image: '/images/d1.jpg',
+        image: '/images/d2.jpg',
         price: 40,
         brand: 'Reliance',
         rating: 3.5,
@@ -24,7 +25,7 @@ export default {
         _id:'3',
         name: 'GupChup',
         Category: 'Fast food',
-        image: '/images/d1.jpg',
+        image: '/images/d3.jpg',
         price: 20,
         brand: 'Reliance',
         rating: 4.0,
@@ -32,12 +33,12 @@ export default {
     },
     {
         _id:'4',
-        name: 'GupChup',
-        Category: 'Fast food',
+        name: 'Tomato',
+        Category: 'Vegetables and fruits',
         image: '/images/d1.jpg',
-        price: 20,
+        price: 60,
         brand: 'Reliance',
-        rating: 4.0,
-        numReviews: 7
+        rating: 4.5,
+        numReviews: 10
     }]
 }
