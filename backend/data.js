10bc956f196c4b707c9ab50ef0bf1c31f12e1e21
dibyarupa jena <dibyathecool.jena@gmail.com>
2020-08-28 06:07:21 +0530

    before deleting node modules

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+    {   
+        _id:'1',
+        name: 'Samosa',
+        Category: 'Fast food',
+        image: '/images/d4.jpg',
+        price: 25,
+        brand: 'Reliance',
+        rating: 3.0,
+        numReviews: 6
+        
+    },
+    {
+        _id:'2',
+        name: 'Potato',
+        Category: 'Vegetables and fruits',
+        image: '/images/d2.jpg',
+        price: 40,
+        brand: 'Reliance',
+        rating: 3.5,
+        numReviews: 9
+    },
+    {
+        _id:'3',
+        name: 'GupChup',
+        Category: 'Fast food',
+        image: '/images/d3.jpg',
+        price: 20,
+        brand: 'Reliance',
+        rating: 4.0,
+        numReviews: 7
+    },
+    {
+        _id:'4',
+        name: 'Tomato',
+        Category: 'Vegetables and fruits',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Reliance',
+        rating: 4.5,
+        numReviews: 10
+    }]
+}
